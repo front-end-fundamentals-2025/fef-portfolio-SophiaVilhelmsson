bfaaeab37f4357d77d39b430a7a1fa98958663f3
SophiaVilhelmsson <viso24wy@student.ju.se>
2025-03-07 08:44:55 +0100

    updated queryselector

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ meElement.style.color = "rgb(239, 160, 0)";
 //Change background color "about me"
 //Got help from ChatGPT to target the first elememt in the class https://chatgpt.com/c/67b8766a-bf3c-8001-814c-a3d794568b84
 
-const backgroundElement = document.getElementsByClassName("about-me-part")[0];
+const backgroundElement = document.querySelector(".about-me-part");
 
 backgroundElement.addEventListener("mouseover", function (event) {
   backgroundElement.style.backgroundColor = "rgb(200, 110, 0)";
